eb78576cca1072924e9fe5d7b066ad50da7c4219
Nishikant Bawache <nishikant.bawache.nb@gmail.com>
2022-10-11 17:29:18 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,48 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <header>
+    <router-link to="/">
+      <h1><span>Cool</span>movies</h1>
+    </router-link>
+  </header>
+  <main>
+    <router-view/>
+  </main>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-size: "Fira sans", sans-serif;
+
+  &::selection{
+    background: transparentize(#42B883, 0.5);
+  }
 }
 
-nav {
-  padding: 30px;
+body{
+  background-color: #35495E;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+a{
+  text-decoration: none;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
+header{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 16px;
+  background-color: #2C3D4E;
+  box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.1);
+
+  h1{
+    color: #fff;
+    font-size: 28px;
+
+    span{
+      color: #42B883;
     }
   }
 }
---
+++ src/env.js
@@ -0,0 +1,3 @@
+export default{
+    apikey: '80b9ad52'
+}
